6210fe6000d4ff80e20e9a2c9bc1725556d2c72f
james <lengjiabing@gmail.com>
2016-03-30 16:28:33 +0800

    update KNN

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -4,6 +4,7 @@
 # @author james::lengjiabing@gmail.com
 from numpy import *
 import operator
+import file2matrix
 
 # the core algorithm calculation code of KNN
 # inX: the input vector;
@@ -28,3 +29,36 @@ def kNNClassify0(inX, dataSet, labels, k):
     sortedClassCount = sorted(classCount.iteritems(),
                             key = operator.itemgetter(1), reverse = True)
     return sortedClassCount[0][0]
+
+# normalization dataSet to let the dataset's value varies from 0 to 1
+# this function can also be extracted out for general purpose
+# the principle is: newValue = (oldValue - min)/(max - min)
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m,1))
+    normDataSet = normDataSet/tile(ranges,(m,1))
+    return normDataSet, ranges, minVals
+
+# testing the KNN algorithm's performance on dating's dataSet
+def datingClassTest():
+    hoRatio = 0.10
+    datingDataMat, datingLabels = file2matrix.file2matrix('datingTestSet.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m * hoRatio)
+    errorCount = 0
+    
+    for i in range(numTestVecs):
+        classifierResult = kNNClassify0(normMat[i,:], normMat[numTestVecs,:],
+                                        datingLabels[numTestVecs:m], 3)
+        print "The classifier came back with : %d, the real answer is : %d" % (
+            classifierResult, datingLabels[i]
+        )
+        if (classifierResult != datingLabels[i]):
+            errorCount +- 1
+
+    print "The Total error rate is : %f" % (errorCount/float(numTestVecs))

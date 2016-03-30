73ab4773b348aed78b8e0f85b2f0c82b6dec3561
james <lengjiabing@gmail.com>
2016-03-30 15:58:07 +0800

    knn

--- KNN/file2matrix.py
+++ KNN/file2matrix.py
@@ -3,6 +3,8 @@
 # this code segment is used to transform the data in a txt file in to a numpy supported matrix.
 # @author james::lengjiabing@gmail.com
 
+from numpy import *
+
 def file2matrix(filename):
     fr = open(filename)
     numberOfLines = len(fr.readlines())         #get the number of lines in the file
@@ -14,7 +16,14 @@ def file2matrix(filename):
         line = line.strip()
         listFromLine = line.split('\t')
         returnMat[index,:] = listFromLine[0:3]
-        classLabelVector.append(int(listFromLine[-1]))
+        tempValue = listFromLine[-1]
+        if tempValue == 'didntLike':
+            classLabelVector.append(1)
+        elif tempValue == 'smallDoes':
+            classLabelVector.append(2)
+        else:
+            classLabelVector.append(3)
+        # classLabelVector.append(listFromLine[-1])
         index += 1
     return returnMat,classLabelVector
 

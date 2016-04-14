22a7e2c13ffa4ffbaf2c94e59611b6d3d1e1b64d
james <lengjiabing@gmail.com>
2016-04-14 16:44:44 +0800

    basic python practiceing code segment

---
+++ pythonlearning/basicPractice.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+print u'这是一段中文字符'
+print '这也是一段中文字符'
+
+print '测试格式化输出'
+
+print '我想输出%d个变量，其中第一个变量是π值%f,第二个变量是我的名字%s。' % (2, 3.14159265358979323846, 'james leng')
+
